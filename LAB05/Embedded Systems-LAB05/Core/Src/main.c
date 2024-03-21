062c3db1174cd36dd5a89f56a1f6e5d352aa61ad
LAPTOP-2O630A78\thuniki <mangasiddharth1772@gmail.com>
2024-03-20 20:34:49 -0600

    LAB05 - CheckOff02

--- LAB05/Embedded Systems-LAB05/Core/Src/main.c
+++ LAB05/Embedded Systems-LAB05/Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -60,54 +59,111 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
- 
-#include "stdio.h"
+void I2C_WriteReg(uint16_t deviceAdd, uint8_t regAdd, uint8_t info);
+int8_t I2C_ReadReg(uint16_t deviceAdd);
+void I2C_SetRegAdd(uint16_t deviceAdd, uint8_t regAdd);
 
-#include "stm32f0xx_hal.h"
+void I2C_SetRegAdd(uint16_t deviceAdd, uint8_t regAdd) {
+  I2C2->CR2 = 0;  // clears register
+  // Using SADD[7:1] bit field in CR2 register to set slave address to addr
+  I2C2->CR2 |= (deviceAdd << 1);
+  // Using NBYTES[7:0] bit field to set number of data bytes to be transmitted to 1
+  I2C2->CR2 |= (0x1 << 16);
+  // Set RD_WRN to WRITE operation - 0 indicates WRITE
+  I2C2->CR2 &= ~(1 << 10);
+  // Set START bit to begin the address frame
+  I2C2->CR2 |= I2C_CR2_START;
 
+  // While TXIS or NACKF flags not set wait
+  while (!(I2C2->ISR & (I2C_ISR_TXIS | I2C_ISR_NACKF))) {
+  }  // getting stuck here on second call to this function! Once TXIS flag set continue
 
-// Gyroscope registers
-#define CTRL_REG1 0x20
-#define OUT_X_L   0x28
-#define OUT_X_H   0x29
-#define OUT_Y_L   0x2A
-#define OUT_Y_H   0x2B
+  // Check if NACK set
+  if (I2C2->ISR & I2C_ISR_NACKF) {
+    // GPIOC->ODR |= GPIO_ODR_6; // RED - I2C not working!
+  }
 
-// LED pins
-#define LED_GREEN GPIO_PIN_9
-#define LED_ORANGE GPIO_PIN_8
-#define LED_RED GPIO_PIN_7
-#define LED_BLUE GPIO_PIN_6
+  // Write data into the TXDR
+  I2C2->TXDR = regAdd;
 
-I2C_HandleTypeDef hi2c1;
-
-void Gyro_Init(void) {
-    uint8_t ctrl_reg1;
-
-    // Enable X and Y axes
-    ctrl_reg1 = 0x38;
-    HAL_I2C_Mem_Write(&hi2c1, 0xD6, CTRL_REG1, I2C_MEMADD_SIZE_8BIT, &ctrl_reg1, 1, 1000);
-
-    // Set normal mode
-    ctrl_reg1 = 0x08;
-    HAL_I2C_Mem_Write(&hi2c1, 0xD6, CTRL_REG1, I2C_MEMADD_SIZE_8BIT, &ctrl_reg1, 1, 1000);
+  // Wait until TC flag set - transfer complete
+  while (!(I2C2->ISR & I2C_ISR_TC)) {
+  }
 }
 
-void Read_Gyro(int16_t *x_data, int16_t *y_data) {
-    uint8_t x_low, x_high, y_low, y_high;
 
-    // Read X axis data
-    HAL_I2C_Mem_Read(&hi2c1, 0xD6, OUT_X_L, I2C_MEMADD_SIZE_8BIT, &x_low, 1, 1000);
-    HAL_I2C_Mem_Read(&hi2c1, 0xD6, OUT_X_H, I2C_MEMADD_SIZE_8BIT, &x_high, 1, 1000);
-    *x_data = (x_high << 8) | x_low;
+void I2C_WriteReg(uint16_t deviceAdd, uint8_t regAdd, uint8_t info) {
+  I2C2->CR2 = 0;  // clear register
+  // Using SADD[7:1] bit field in CR2 register to set slave address to addr
+  I2C2->CR2 |= (deviceAdd << 1);
+  // Using NBYTES[7:0] bit field to set number of data bytes to be transmitted to
+  // 1
+  I2C2->CR2 |= (0x2 << 16);
+  // Set RD_WRN to WRITE operation - 0 indicates WRITE
+  I2C2->CR2 &= ~(1 << 10);
+  // Set START bit to begin the address frame
+  I2C2->CR2 |= I2C_CR2_START;
 
-    // Read Y axis data
-    HAL_I2C_Mem_Read(&hi2c1, 0xD6, OUT_Y_L, I2C_MEMADD_SIZE_8BIT, &y_low, 1, 1000);
-    HAL_I2C_Mem_Read(&hi2c1, 0xD6, OUT_Y_H, I2C_MEMADD_SIZE_8BIT, &y_high, 1, 1000);
-    *y_data = (y_high << 8) | y_low;
+  // While TXIS or NACKF flags not set wait
+  while (!(I2C2->ISR & (I2C_ISR_TXIS | I2C_ISR_NACKF))) {
+  }  // getting stuck here on second call to this function!Once TXIS flag set continue
+
+  // Check if NACK set
+  if (I2C2->ISR & I2C_ISR_NACKF) {
+    // GPIOC->ODR |= GPIO_ODR_6; // RED - I2C not working!
+  }
+
+  // Set reg address
+  I2C2->TXDR = regAdd;
+
+  while (!(I2C2->ISR & I2C_ISR_TXIS)) {
+  }
+
+  // Write data into the TXDR
+  I2C2->TXDR = info;
+
+  // Wait until TC flag set - transfer complete
+  while (!(I2C2->ISR & I2C_ISR_TC)) {
+  }
 }
+
+int8_t I2C_ReadReg(uint16_t deviceAdd) {
+  I2C2->CR2 = 0;  // clear register
+  int8_t info = 0;
+
+  // SADD[7:1] bit field in CR2 register to set slave address to L3GD20
+  I2C2->CR2 |= (deviceAdd << 1);
+  //  NBYTES[7:0] bit field to set number of data bytes to be transmitted to 1
+  
+  I2C2->CR2 |= (0x1 << 16);
+  // RD_WRN to READ operation - 1 indicates READ
+  I2C2->CR2 |= (1 << 10);
+  // START bit to begin the address frame
+  I2C2->CR2 |= I2C_CR2_START;
+
+  // While RXNE or NACKF flags not set wait
+  while (!(I2C2->ISR & (I2C_ISR_RXNE | I2C_ISR_NACKF))) {
+  }
+  // Once RXNE flag set continue
+
+  // Check if NACK set
+  if (I2C2->ISR & I2C_ISR_NACKF) {
+    GPIOC->ODR |= GPIO_ODR_8;  // ORANGE - I2C not working!
+  }
+
+  // Wait for TC flag set
+  while (!(I2C2->ISR & I2C_ISR_TC)) {
+  }
+
+  // Read contents of RXDR register and return data - remember it is 1 byte at a
+  // time
+  info = I2C2->RXDR;
+  return info;
+}
+
 int main(void)
 {
+    int32_t threshold = 1250;
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -117,20 +173,29 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
- 
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
   /* Configure the system clock */
   SystemClock_Config();
+	// Enable GPIOB clock
+	 RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+  // Enable GPIOC clock
+   RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+	
+	 // Configure-PC6-and-PC7-as-output
 
-  /* USER CODE BEGIN SysInit */
+    GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0); // Output mode
+    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7); // Push-pull 
+	  GPIOC->OSPEEDR |= (GPIO_OSPEEDR_OSPEEDR6 | GPIO_OSPEEDR_OSPEEDR7); // High speed 
+	  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6 | GPIO_PUPDR_PUPDR7); // No pull-up, no pull-down
+	 // Configure-PC8-and-PC9-as-output
 
-  /* USER CODE END SysInit */
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-  // Enable GPIOB
-  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
- 
-  // Enable GPIOC
-  RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    GPIOC->MODER |= (GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0); // Output mode
+    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9); // Push-pull 
+	  GPIOC->OSPEEDR |= (GPIO_OSPEEDR_OSPEEDR8 | GPIO_OSPEEDR_OSPEEDR9); // High speed 
+	  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8 | GPIO_PUPDR_PUPDR9); // No pull-up, no pull-down
 	
 	 // Configure PB11 in alternate function mode
     GPIOB->MODER &= ~(GPIO_MODER_MODER11);  // Clear the bits
@@ -150,9 +215,9 @@ int main(void)
     // Configure PB11 as open-drain output
     GPIOB->OTYPER |= GPIO_OTYPER_OT_13;
 
-    // Configure the alternate function for PB13 (assuming it's AF4 for I2C2_SDA, check your datasheet/reference manual)
+    // Configure the alternate function for PB13 it's AF5 for I2C2_SDA
     GPIOB->AFR[1] &= ~GPIO_AFRH_AFSEL13; // Clear the bits
-    GPIOB->AFR[1] |= (5 << GPIO_AFRH_AFSEL13_Pos); // Set AF4 for I2C2_SCL
+    GPIOB->AFR[1] |= (5 << GPIO_AFRH_AFSEL13_Pos); // Set AF5 for I2C2_SCL
  
     // Configure PB14 in general purpose output mode
     GPIOB->MODER &= ~GPIO_MODER_MODER14;  // Clear the bits
@@ -183,129 +248,48 @@ int main(void)
     // Enable the I2C2 peripheral using the PE bit in the CR1 register
     I2C2->CR1 |= I2C_CR1_PE;
 		
-	 // Configure-PC6-and-PC7-as-output
-
-    GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0); // Output mode
-    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7); // Push-pull 
-	  GPIOC->OSPEEDR |= (GPIO_OSPEEDR_OSPEEDR6 | GPIO_OSPEEDR_OSPEEDR7); // High speed 
-	  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6 | GPIO_PUPDR_PUPDR7); // No pull-up, no pull-down
-	
-  	// Configure-PC8-and-PC9-as-output
-
-    GPIOC->MODER |= (GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0); // Output mode
-    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9); // Push-pull 
-	  GPIOC->OSPEEDR |= (GPIO_OSPEEDR_OSPEEDR8 | GPIO_OSPEEDR_OSPEEDR9); // High speed 
-	  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8 | GPIO_PUPDR_PUPDR9); // No pull-up, no pull-down
-	
-    GPIOC->ODR ^= GPIO_ODR_8;
-	  GPIOC->ODR ^= GPIO_ODR_9;
-    GPIOC->ODR ^= GPIO_ODR_6;
-	  GPIOC->ODR ^= GPIO_ODR_7;	
+		I2C_WriteReg(0x69, 0x20, 0xB);
 		
-		
-		
-		// Step 1: Set transaction parameters in CR2 register
-    I2C2->CR2 = (0x69 << 1) | (1 << 16) | I2C_CR2_START;
-     
-    // Step 2: Wait until either TXIS or NACKF flags are set
-    while (!(I2C2->ISR & (I2C_ISR_TXIS | I2C_ISR_NACKF))) {
-      // Wait
+		 while (1) {
+
+    I2C_SetRegAdd(0x69, 0x28);
+    int8_t lowx = I2C_ReadReg(0x69);
+    I2C_SetRegAdd(0x69, 0x29);
+    int8_t highx = I2C_ReadReg(0x69);
+    int16_t xd = ((int16_t)highx << 8) | (uint8_t)lowx;
+
+    I2C_SetRegAdd(0x69, 0x2A);
+    int8_t lowy = I2C_ReadReg(0x69);
+    I2C_SetRegAdd(0x69, 0x2B);
+    int8_t highy = I2C_ReadReg(0x69);
+    int16_t yd = ((int16_t)highy << 8) | (uint8_t)lowy;
+
+    GPIOC->ODR &= ~(GPIO_ODR_7 | GPIO_ODR_6 | GPIO_ODR_8 |
+                    GPIO_ODR_9);  // Reset the ODR bits for LEDs
+
+    if (yd > threshold) 
+		{
+      GPIOC->ODR |= GPIO_ODR_6;  // Red LED for positive Y
+    } 
+		else if (yd < -threshold) 
+		{
+      GPIOC->ODR |= GPIO_ODR_7;  // Blue LED for negative Y
     }
 
-    // If NACKF flag is set, there might be an error
-    if (I2C2->ISR & I2C_ISR_NACKF) {
-     GPIOC->ODR ^= GPIO_ODR_6;   // Handle error (e.g., print error message or set LED pattern)   
+    if (xd > threshold) 
+		{
+      GPIOC->ODR |= GPIO_ODR_9;  // Green LED for positive X
+    } 
+		else if (xd < -threshold) 
+		{
+      GPIOC->ODR |= GPIO_ODR_8;  // Orange LED for negative X
     }
 
-    // Step 3: Write the address of the "WHO_AM_I" register into TXDR
-    I2C2->TXDR = 0x0F;
+    HAL_Delay(300);
+  }	
 
-    // Step 4: Wait until TC flag is set
-    while (!(I2C2->ISR & I2C_ISR_TC)) {
-        // Wait
-    }
-
-    // Step 5: Reload CR2 register with read operation parameters and set START bit
-    I2C2->CR2 = (0x69 << 1) | (1 << 16) | I2C_CR2_RD_WRN | I2C_CR2_START;
-
-    // Step 6: Wait until either RXNE or NACKF flags are set
-    while (!(I2C2->ISR & (I2C_ISR_RXNE | I2C_ISR_NACKF))) {
-        // Wait
-    }
-
-    // If NACKF flag is set, there might be an error
-    if (I2C2->ISR & I2C_ISR_NACKF) {
-        GPIOC->ODR ^= GPIO_ODR_8; // Handle error (e.g., print error message or set LED pattern)
-    }
-
-    // Step 7: Wait until TC flag is set
-    while (!(I2C2->ISR & I2C_ISR_TC)) {
-        // Wait
-    }
-
-    // Step 8: Check the contents of RXDR register
-    uint8_t received_data = I2C2->RXDR;
-    if (received_data == 0xD3) {
-        // WHO_AM_I register value matches the expected value
-       GPIOC->ODR ^= GPIO_ODR_7;  // Proceed with further actions (e.g., print message or set LED pattern)
-    } else {
-        // WHO_AM_I register value doesn't match the expected value
-       GPIOC->ODR ^= GPIO_ODR_9;  // Handle error (e.g., print error message or set LED pattern)
-    }
-
-    // Step 9: Set the STOP bit in CR2 register
-    I2C2->CR2 |= I2C_CR2_STOP;  
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-		
-   int16_t x_data, y_data;
-    uint32_t led_state = 0;
-
-// Initialize gyroscope
-    Gyro_Init();
-		
-   // Initialize I2C
-    hi2c1.Instance = I2C1;
-    hi2c1.Init.Timing = 0x00300F38; // Set appropriate timing value
-    hi2c1.Init.OwnAddress1 = 0;
-    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c1.Init.OwnAddress2 = 0;
-    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
-        // Error handling
-			  Error_Handler();
-    }
-   // Enable I2C peripheral clock
-    __HAL_RCC_I2C1_CLK_ENABLE();
-
-    while (1) {
-        Read_Gyro(&x_data, &y_data);
- // Clear previous LED state
-        led_state = 0;
-
-        // Update LED state based on gyroscope data
-        if (x_data > 500) {
-            led_state |= LED_ORANGE;
-        } else if (x_data < -500) {
-            led_state |= LED_RED;
-        }
-
-        if (y_data > 500) {
-            led_state |= LED_GREEN;
-        } else if  (y_data < -500) {
-            led_state |= LED_BLUE;
-        }
-
-        // Set LED state
-        GPIOC->BSRR = (led_state << 16) | (~led_state & 0);
-
-        // Delay for 100 ms
-        HAL_Delay(100);
-    }
 }
+
 
 /**
   * @brief System Clock Configuration
